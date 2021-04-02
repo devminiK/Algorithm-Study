beee4af08b4421af709caeaee886b7f28974d17f
Sungmin Kim <50899073+devminiK@users.noreply.github.com>
2021-04-02 16:30:30 +0900

    Update Array169.java
    줄 바꿈

--- Array169.java
+++ Array169.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 class Solution {
+	public static int majorityElement(int[] nums) {
+    		int result = 0;
+    	
+		Arrays.sort(nums);
+		for(int i=0 ;i<nums.length; i++) {
+			if(nums.length/2 == i) {
+				result = nums[i];
+			}
+		}
+		return  result;
+    	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
@@ -10,17 +22,4 @@ class Solution {
 		int[] splitNum = Stream.of(str.split(",")).mapToInt(Integer::parseInt).toArray();
 		System.out.println(majorityElement(splitNum));
 	}
-	
-    public static int majorityElement(int[] nums) {
-    	int result = 0;
-    	
-		Arrays.sort(nums);
-		for(int i=0 ;i<nums.length; i++) {
-			if(nums.length/2 == i) {
-				result = nums[i];
-			}
-			
-		}
-		return  result;
-    }
 }
