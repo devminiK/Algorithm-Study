4e9e8c1c24ef3ee1ccf8633be56e54f8c6b75965
kimsungmin <sungminomg97@gmail.com>
2021-04-02 16:24:21 +0900

    Array 169 my Solution

---
+++ Array169.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		str = str.substring(8);
+		str = str.substring(0,str.length()-1);
+		
+		int[] splitNum = Stream.of(str.split(",")).mapToInt(Integer::parseInt).toArray();
+		System.out.println(majorityElement(splitNum));
+	}
+	
+    public static int majorityElement(int[] nums) {
+    	int result = 0;
+    	
+		Arrays.sort(nums);
+		for(int i=0 ;i<nums.length; i++) {
+			if(nums.length/2 == i) {
+				result = nums[i];
+			}
+			
+		}
+		return  result;
+    }
+}
