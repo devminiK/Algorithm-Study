cdb8a6aa21b7389013e8914b3608b662e4c69718
Sungmin Kim <50899073+devminiK@users.noreply.github.com>
2021-04-14 10:01:57 +0900

    Update Array448_FindAllNumbersDisapperedInAnArray.java

--- Array/Array448_FindAllNumbersDisapperedInAnArray.java
+++ Array/Array448_FindAllNumbersDisapperedInAnArray.java
@@ -6,31 +6,27 @@
     - 1 <= n <= 105
     - 1 <= nums[i] <= n
 */
-
-class Solution {
-  public static List<Integer> findDisappearedNumbers(int[] nums) {
+	public static List<Integer> findDisappearedNumbers(int[] nums) {
 		/*
 		 * 1) TreeSet을 이용한 중복제거 및 정렬시도->시간 제한문제
-		 * 2) 직접 정렬 및 중복 제거-> 시간 제한문제
-     * 3) 값 존재 여부 따지기 -> 성공
+		 * 2) 직접 정렬 및 중복 제거-> 시간 문제
+		 * 3) 값 존재 여부 따지기 -> 성공
 		 * */
-		
-    List<Integer> res = new ArrayList<Integer>();	//결과값 
+		List<Integer> res = new ArrayList<Integer>();	//결과값 
 		if(nums == null || nums.length == 0) {
-      return res;
+			return res;
 		}
 		
 		boolean[] tmp = new boolean[nums.length];
 		for(int i=0;i<nums.length; i++) {
 			tmp[nums[i]-1] = true;
 		}
+		
 
 		for (int i=0; i<nums.length; i++) {
 			if(!tmp[i]) {
 				res.add(i+1);
 			}
-			
 		}
 		return res;	
 	}
-}
